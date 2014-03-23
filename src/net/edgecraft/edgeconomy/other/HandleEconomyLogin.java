667c8daf86698a7c723618aa4630d2c44ee55f94
EdgeCraft <panjab@edgecraft.de>
2014-03-23 12:35:07 +0100

    Automatical account creation

--- src/net/edgecraft/edgeconomy/other/HandleEconomyLogin.java
+++ src/net/edgecraft/edgeconomy/other/HandleEconomyLogin.java
@@ -1,11 +1,13 @@
 package net.edgecraft.edgeconomy.other;
 
+import net.edgecraft.edgeconomy.EdgeConomy;
 import net.edgecraft.edgeconomy.EdgeConomyAPI;
 import net.edgecraft.edgeconomy.economy.Economy;
 import net.edgecraft.edgecore.EdgeCoreAPI;
 import net.edgecraft.edgecore.lang.LanguageHandler;
 import net.edgecraft.edgecore.user.User;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -17,7 +19,7 @@ public class HandleEconomyLogin implements Listener {
 	private final LanguageHandler lang = EdgeCoreAPI.languageAPI();
 	private final Economy economy = EdgeConomyAPI.economyAPI();
 	
-	@EventHandler(priority = EventPriority.LOW)
+	@EventHandler
 	public void registerEconomyPlayer(PlayerJoinEvent event) {
 		
 		Player player = event.getPlayer();
@@ -40,11 +42,11 @@ public class HandleEconomyLogin implements Listener {
 		}
 	}
 	
-	@EventHandler
+	@EventHandler(priority = EventPriority.HIGH)
 	public void registerAccount(PlayerJoinEvent event) {
 		
-		Player player = event.getPlayer();
-		User user = EdgeCoreAPI.userAPI().getUser(player.getName());
+		final Player player = event.getPlayer();
+		final User user = EdgeCoreAPI.userAPI().getUser(player.getName());
 		
 		if (user == null)
 			return;
@@ -56,12 +58,22 @@ public class HandleEconomyLogin implements Listener {
 			
 			if (Economy.isAutoCreateAccounts()) {
 				
+				if (economy.hasAccount(user.getID()))
+					return;
+				
 				economy.registerAccount(user.getID(), 500D, 0);
 				player.sendMessage(lang.getColoredMessage(user.getLanguage(), "acc_apply_success").replace("[0]", economy.getAccount(player.getName()).getID() + ""));
 				
 			} else {
 				
-				player.sendMessage(lang.getColoredMessage(user.getLanguage(), "registration_eco_accinfo"));
+				Bukkit.getScheduler().runTaskTimer(EdgeConomy.getInstance(), new Runnable() {
+					
+					@Override
+					public void run() {
+						player.sendMessage(lang.getColoredMessage(user.getLanguage(), "registration_eco_accinfo"));
+					}
+					
+				}, 20L * 5, 20L * 60 * 15);
 				
 			}
 			
